9daeb7f4d0a0ef44cfc071afcece6e422e1a1c0a
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-06-29 22:20:27 +0430

    Serve http handler in server command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/mammadmodi/detective/internal/config"
+	"github.com/mammadmodi/detective/internal/handler"
 	"github.com/mammadmodi/detective/pkg/logger"
 	"go.uber.org/zap"
+	"net/http"
+	"os"
 )
 
 var c *config.AppConfig
 var l *zap.Logger
+var hc *http.Client
 
 func init() {
 	var err error
@@ -20,7 +24,26 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	hc = &http.Client{
+		Timeout: c.HTTPTimeout,
+	}
+
+	l.With(zap.Any("configs", c)).Info("application initialized successfully")
 }
+
 func main() {
-	// TODO Run the web server.
+	// Create http server.
+	h := handler.New(l, hc)
+	server := &http.Server{
+		Addr:    c.Addr,
+		Handler: h.GetRouter(),
+	}
+
+	// Launch server and listen to application port.
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		l.With(zap.Error(err)).Panic("error while running gin http server, error: %v")
+	}
+
+	os.Exit(0)
 }

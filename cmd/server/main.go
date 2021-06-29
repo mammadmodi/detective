cefced61e1029f60d6273c36f5bb910417032e32
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-06-29 20:04:05 +0430

    Add init function in server command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"github.com/mammadmodi/webpage-analyzer/internal/config"
+	"github.com/mammadmodi/webpage-analyzer/pkg/logger"
+	"go.uber.org/zap"
+)
+
+var c *config.AppConfig
+var l *zap.Logger
+
+func init() {
+	var err error
+	c, err = config.NewAppConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	l, err = logger.NewZapLogger("webpage_analyzer", c.LoggerConfig)
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
+	// TODO Run the web server.
 }

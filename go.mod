295d820d4a60d9108b97707e6786068b4c8303a7
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-07-02 16:56:30 +0430

    Add unit tests for config package

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.18.0
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 )
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,7 +1,70 @@
 package config
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
 
-func TestNewAppConfig(t *testing.T) {
-	// TODO check integrity of env variable and processed configs.
+	"github.com/mammadmodi/detective/pkg/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+// setConfigOsEnvVariables creates a template AppConfig and sets it's related env variables to OS.
+func setConfigOsEnvVariables() *AppConfig {
+	c := &AppConfig{
+		LoggerConfig: &logger.Config{
+			Enabled:             true,
+			Level:               "WARN",
+			Pretty:              true,
+			FileRedirectEnabled: true,
+			FileRedirectPath:    "/var/log",
+			FileRedirectPrefix:  "detective",
+		},
+		Addr:        "10.0.0.1:8080",
+		HTTPTimeout: 25 * time.Second,
+	}
+
+	_ = os.Setenv("DETECTIVE_LOGGER_ENABLED", fmt.Sprint(c.LoggerConfig.Enabled))
+	_ = os.Setenv("DETECTIVE_LOGGER_LEVEL", c.LoggerConfig.Level)
+	_ = os.Setenv("DETECTIVE_LOGGER_PRETTY", fmt.Sprint(c.LoggerConfig.Pretty))
+	_ = os.Setenv("DETECTIVE_LOGGER_FILE_REDIRECT_ENABLED", fmt.Sprint(c.LoggerConfig.FileRedirectEnabled))
+	_ = os.Setenv("DETECTIVE_LOGGER_FILE_REDIRECT_PATH", c.LoggerConfig.FileRedirectPath)
+	_ = os.Setenv("DETECTIVE_LOGGER_FILE_REDIRECT_PREFIX", c.LoggerConfig.FileRedirectPrefix)
+	_ = os.Setenv("DETECTIVE_ADDR", c.Addr)
+	_ = os.Setenv("DETECTIVE_HTTP_TIMEOUT", c.HTTPTimeout.String())
+
+	return c
+}
+
+func TestNewConfigurationSuccess(t *testing.T) {
+	expectedConfigs := setConfigOsEnvVariables()
+	actualConfigs, err := NewAppConfig()
+	if !assert.NoError(t, err) {
+		t.Error(fmt.Errorf("error while loading configurations, error: %v", err))
+		return
+	}
+
+	assert.NotNil(t, actualConfigs)
+	assert.EqualValues(t, expectedConfigs, actualConfigs)
+}
+
+func TestNewConfigurationFailures(t *testing.T) {
+	t.Run("error when logger config is not valid", func(t *testing.T) {
+		// Enabled field must be boolean.
+		_ = os.Setenv("DETECTIVE_LOGGER_ENABLED", "invalid_type")
+		c, err := NewAppConfig()
+
+		assert.Nil(t, c)
+		assert.Error(t, err)
+	})
+
+	t.Run("error when http client timeout value is not valid", func(t *testing.T) {
+		// Port must be integer
+		_ = os.Setenv("DETECTIVE_HTTP_TIMEOUT", "invalid_duration")
+		c, err := NewAppConfig()
+
+		assert.Nil(t, c)
+		assert.Error(t, err)
+	})
 }

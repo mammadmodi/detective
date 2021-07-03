cfa88f5b0cef188d1e6aa1e69b8a5470a1ef6854
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-07-04 04:01:33 +0430

    Set default value of pretty field to true

--- pkg/logger/l.go
+++ pkg/logger/l.go
@@ -4,7 +4,7 @@ package logger
 type Config struct {
 	Enabled             bool   `default:"true" split_words:"true"`
 	Level               string `default:"info" split_words:"true"`
-	Pretty              bool   `default:"false" split_words:"true"`
+	Pretty              bool   `default:"true" split_words:"true"`
 	FileRedirectEnabled bool   `default:"false" split_words:"true"`
 	FileRedirectPath    string `default:"/var/log" split_words:"true"`
 	FileRedirectPrefix  string `default:"detective" split_words:"true"`
